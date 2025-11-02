1f93ea546a0eaa638dd62d452e56a73f675a7c88
Serhii Taran <serhii.taran@planetvpn.dev>
2025-11-02 21:32:18 +0200

    hw/18.1 Intro to Cypress, configure cypress.config.js file

---
+++ cypress.config.js
@@ -0,0 +1,30 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      return config;
+    },
+
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
+    requestTimeout: 10000,
+    responseTimeout: 30000,
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
+    video: true,
+    videoCompression: 32,
+    screenshotOnRunFailure: true,
+    trashAssetsBeforeRuns: true,
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos",
+    chromeWebSecurity: false,
+    experimentalMemoryManagement: true,
+    numTestsKeptInMemory: 10,
+    watchForFileChanges: true,
+  },
+});
